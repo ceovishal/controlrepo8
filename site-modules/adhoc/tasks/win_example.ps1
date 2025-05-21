e130c48096fad4f4eb3cc169f388920b3cfa5a30
Vishal Kharade <37978213+ceovishal@users.noreply.github.com>
2025-05-21 12:24:50 +0530

    Update win_example.ps1

--- site-modules/adhoc/tasks/win_example.ps1
+++ site-modules/adhoc/tasks/win_example.ps1
@@ -1,1 +1,10 @@
-puppet resource package
+# win_example.ps1
+
+Write-Output "Running puppet resource package..."
+
+# Run the 'puppet resource package' command
+$package_output = puppet resource package
+
+# Output the result to verify what's happening
+Write-Output "Output of puppet resource package:"
+Write-Output $package_output
